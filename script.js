64f9e86aaca4b70efafa7ac9baba7dc1800ec438
V3GXVMU <226399187+v36x@users.noreply.github.com>
2026-01-15 17:17:40 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function TOOL() {
+    document.getElementById("TLROOM").style.display = "block";
+    document.getElementById("contant").style.display = "none";
+  }
+
+function PC() {
+    document.getElementById("CLROOM").style.display = "block";
+    document.getElementById("contant").style.display = "none";
+  }
+
+  function Home(){
+    document.getElementById("CLROOM").style.display = "none";
+    document.getElementById("TLROOM").style.display = "none";
+    document.getElementById("contant").style.display = "block";    
+  }
 
